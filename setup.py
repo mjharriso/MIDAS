949a9fa04e173fc31f88976d3f460607098f2d1b
Matthew Harrison <matthew.harrison@noaa.gov>
2013-10-31 23:52:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,30 @@
+"""
+==============================
+
+ MIDAS specializes in handling data on a 2-dimensional
+ lattice of grid cells lying in a 
+ cartesian space or on a sphere.  The vertical
+ coordinate accomodates generalized surfaces
+ i.e. surfaces which are static dynamic in time.
+
+ MIDAS (Modular Isosurface Data Analysis System)
+ was first developed by Matthew Harrison
+ in 2011-2012 as an employee of NOAA in the 
+ GFDL Oceans and Climate Group. MIDAS is being made readily
+ available through Github.  #NOAA#GFDL
+ 
+
+ This work is licensed under the Creative Commons
+ Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+ To view a copy of this license, visit   
+ http://creativecommons.org/licenses/by-nc-sa/3.0/
+ or send a letter to Creative Commons, 444 Castro Street,
+ Suite 900, Mountain View, California, 94041, USA.
+
+===============================
+
+
+
 from numpy.distutils.core import Extension
 
 remap_sfc_fluxes = Extension(name = 'midas.remap_sfc_fluxes',
