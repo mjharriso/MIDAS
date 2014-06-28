85c1efc86eacd0a297156f1286aff081f65ac0db
Matthew Harrison <matthew.harrison@noaa.gov>
2014-06-28 06:03:04 -0400

    Added example volume integral calculation using WOA data.

---
+++ examples/volume_integrals.py
@@ -0,0 +1,13 @@
+from midas import *
+
+
+grid=rectgrid('http://data.nodc.noaa.gov/thredds/dodsC/woa/WOA09/NetCDFdata/temperature_annual_1deg.nc',var='t_an',cyclic=True)
+IO = grid.geo_region(x=(30,120.),y=(-30.,25.))
+S=state('http://data.nodc.noaa.gov/thredds/dodsC/woa/WOA09/NetCDFdata/temperature_monthly_1deg.nc',grid=grid,geo_region=IO,fields=['t_an'],default_calendar='noleap')
+
+S.time_avg('t_an')
+S.volume_integral('t_an_tav','XYZ',normalize=False)
+
+rho=1.e3;cp=3989.
+print 'Total heat content (J) = ',rho*cp*S.t_an_tav_xyzint[0]
+
