735a8bdc297e3401e79740a6cef92cfe2645bcaa
Matthew Harrison <Matthew.Harrison@noaa.gov>
2017-12-01 11:13:53 -0500

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 
 make -f Makefile_gfortran fms_build/libfms.a
-#make -f Makefile_gfortran MOM6_ALE/build_ale/libale.a
+make -f Makefile_gfortran MOM6_ALE/build_ale/libale.a
 $PYTHON setup.py config_fc --f90flags="-i4 -r8 -DPY_SOLO" --fcompiler=gfortran \
           --f90flags="-fcray-pointer -fdefault-real-8 \
           -ffixed-line-length-132 -ffree-line-length-0 -DPY_SOLO" build
